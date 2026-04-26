e967ecdc53ee2ce69b9ce6e11674b684a78e60f9
Admin <toandn9x@gmail.com>
2026-04-26 10:28:17 +0700

    Update Remix Icon to latest version to fix missing TikTok icon

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -5,6 +5,7 @@
 <link href="{{ URL::asset('assets/css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
 <link href="{{ URL::asset('assets/css/icons.css') }}" rel="stylesheet" type="text/css" />
+<link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet" />
 <!-- App Css-->
 <link href="{{ URL::asset('assets/css/app.css') }}" rel="stylesheet" type="text/css" />
 <!-- custom Css-->
